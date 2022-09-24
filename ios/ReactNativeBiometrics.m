998456f145a000668ce42b9feb28f40f023d8343
Dan Loman <daniel.h.loman@gmail.com>
2022-09-24 08:59:12 -0700

    Move to Curve25519 keys & store as generic password

--- ios/ReactNativeBiometrics.m
+++ ios/ReactNativeBiometrics.m
@@ -64,36 +64,30 @@ RCT_EXPORT_METHOD(createKeys: (NSDictionary *)params resolver:(RCTPromiseResolve
       return;
     }
 
-    NSData *biometricKeyTag = [self getBiometricKeyTag];
+    Keys* keys = [Curve25519 generateKeypair];
+
+    NSData *biometricKeyService = [self getBiometricKeyService];
     NSDictionary *keyAttributes = @{
-                                    (id)kSecClass: (id)kSecClassKey,
-                                    (id)kSecAttrKeyType: (id)kSecAttrKeyTypeRSA,
-                                    (id)kSecAttrKeySizeInBits: @2048,
-                                    (id)kSecPrivateKeyAttrs: @{
-                                        (id)kSecAttrIsPermanent: @YES,
-                                        (id)kSecUseAuthenticationUI: (id)kSecUseAuthenticationUIAllow,
-                                        (id)kSecAttrApplicationTag: biometricKeyTag,
-                                        (id)kSecAttrAccessControl: (__bridge_transfer id)sacObject
-                                        }
+                                    (id)kSecClass: (id)kSecClassGenericPassword,
+                                    (id)kSecAttrService: biometricKeyService,
+                                    (id)kSecAttrAccessControl: (__bridge_transfer id)sacObject,
+                                    (id)kSecValueData: (id)keys.privateKey
                                     };
 
     [self deleteBiometricKey];
-    NSError *gen_error = nil;
-    id privateKey = CFBridgingRelease(SecKeyCreateRandomKey((__bridge CFDictionaryRef)keyAttributes, (void *)&gen_error));
 
-    if(privateKey != nil) {
-      id publicKey = CFBridgingRelease(SecKeyCopyPublicKey((SecKeyRef)privateKey));
-      CFDataRef publicKeyDataRef = SecKeyCopyExternalRepresentation((SecKeyRef)publicKey, nil);
-      NSData *publicKeyData = (__bridge NSData *)publicKeyDataRef;
-      NSData *publicKeyDataWithHeader = [self addHeaderPublickey:publicKeyData];
-      NSString *publicKeyString = [publicKeyDataWithHeader base64EncodedStringWithOptions:0];
+    NSError *storage_error = nil;
+    OSStatus status = SecItemAdd((__bridge CFDictionaryRef)keyAttributes, (void *)&storage_error);
+
+    if(keys.publicKey != nil) {
+      NSString *publicKeyString = [keys.publicKey base64EncodedStringWithOptions:0];
 
       NSDictionary *result = @{
         @"publicKey": publicKeyString,
       };
       resolve(result);
     } else {
-      NSString *message = [NSString stringWithFormat:@"Key generation error: %@", gen_error];
+      NSString *message = [NSString stringWithFormat:@"Key storage error: %@", storage_error];
       reject(@"storage_error", message, nil);
     }
   });
@@ -129,21 +123,20 @@ RCT_EXPORT_METHOD(createSignature: (NSDictionary *)params resolver:(RCTPromiseRe
     NSString *promptMessage = [RCTConvert NSString:params[@"promptMessage"]];
     NSString *payload = [RCTConvert NSString:params[@"payload"]];
 
-    NSData *biometricKeyTag = [self getBiometricKeyTag];
+    NSData *biometricKeyService = [self getBiometricKeyService];
     NSDictionary *query = @{
-                            (id)kSecClass: (id)kSecClassKey,
-                            (id)kSecAttrApplicationTag: biometricKeyTag,
-                            (id)kSecAttrKeyType: (id)kSecAttrKeyTypeRSA,
-                            (id)kSecReturnRef: @YES,
                             (id)kSecUseOperationPrompt: promptMessage
+                            (id)kSecClass: (id)kSecClassGenericPassword,
+                            (id)kSecAttrService: biometricKeyService,
+                            (id)kSecReturnData: @YES,
                             };
-    SecKeyRef privateKey;
-    OSStatus status = SecItemCopyMatching((__bridge CFDictionaryRef)query, (CFTypeRef *)&privateKey);
+    NSData *privateKey;
+    OSStatus status = SecItemCopyMatching((__bridge CFDictionaryRef)query, (CFDataRef *)&privateKey);
 
     if (status == errSecSuccess) {
       NSError *error;
       NSData *dataToSign = [payload dataUsingEncoding:NSUTF8StringEncoding];
-      NSData *signature = CFBridgingRelease(SecKeyCreateSignature(privateKey, kSecKeyAlgorithmRSASignatureMessagePKCS1v15SHA256, (CFDataRef)dataToSign, (void *)&error));
+      NSData *signature = [Curve25519 signatureForPayload:dataToSign privateKey:privateKey error:&error];
 
       if (signature != nil) {
         NSString *signatureString = [signature base64EncodedStringWithOptions:0];
@@ -223,19 +216,16 @@ RCT_EXPORT_METHOD(biometricKeysExist: (RCTPromiseResolveBlock)resolve rejecter:(
   });
 }
 
-- (NSData *) getBiometricKeyTag {
-  NSString *biometricKeyAlias = @"com.rnbiometrics.biometricKey";
-  NSData *biometricKeyTag = [biometricKeyAlias dataUsingEncoding:NSUTF8StringEncoding];
-  return biometricKeyTag;
+- (NSString *) getBiometricKeyService {
+  return @"com.rnbiometrics.biometricKey";
 }
 
 - (BOOL) doesBiometricKeyExist {
-  NSData *biometricKeyTag = [self getBiometricKeyTag];
+  NSString *biometricKeyService = [self getBiometricKeyService];
   NSDictionary *searchQuery = @{
-                                (id)kSecClass: (id)kSecClassKey,
-                                (id)kSecAttrApplicationTag: biometricKeyTag,
-                                (id)kSecAttrKeyType: (id)kSecAttrKeyTypeRSA,
                                 (id)kSecUseAuthenticationUI: (id)kSecUseAuthenticationUIFail
+                                (id)kSecClass: (id)kSecClassGenericPassword,
+                                (id)kSecAttrService: biometricKeyService,
                                 };
 
   OSStatus status = SecItemCopyMatching((__bridge CFDictionaryRef)searchQuery, nil);
@@ -243,11 +233,10 @@ RCT_EXPORT_METHOD(biometricKeysExist: (RCTPromiseResolveBlock)resolve rejecter:(
 }
 
 -(OSStatus) deleteBiometricKey {
-  NSData *biometricKeyTag = [self getBiometricKeyTag];
+  NSString *biometricKeyService = [self getBiometricKeyService];
   NSDictionary *deleteQuery = @{
-                                (id)kSecClass: (id)kSecClassKey,
-                                (id)kSecAttrApplicationTag: biometricKeyTag,
-                                (id)kSecAttrKeyType: (id)kSecAttrKeyTypeRSA
+                                (id)kSecClass: (id)kSecClassGenericPassword,
+                                (id)kSecAttrService: biometricKeyService,
                                 };
 
   OSStatus status = SecItemDelete((__bridge CFDictionaryRef)deleteQuery);
@@ -288,68 +277,6 @@ RCT_EXPORT_METHOD(biometricKeysExist: (RCTPromiseResolveBlock)resolve rejecter:(
   }
 
   return message;
-}
-
-
-- (NSData *)addHeaderPublickey:(NSData *)publicKeyData {
-
-    unsigned char builder[15];
-    NSMutableData * encKey = [[NSMutableData alloc] init];
-    unsigned long bitstringEncLength;
-
-    static const unsigned char _encodedRSAEncryptionOID[15] = {
-
-        /* Sequence of length 0xd made up of OID followed by NULL */
-        0x30, 0x0d, 0x06, 0x09, 0x2a, 0x86, 0x48, 0x86,
-        0xf7, 0x0d, 0x01, 0x01, 0x01, 0x05, 0x00
-
-    };
-    // When we get to the bitstring - how will we encode it?
-    if  ([publicKeyData length ] + 1  < 128 )
-        bitstringEncLength = 1 ;
-    else
-        bitstringEncLength = (([publicKeyData length ] +1 ) / 256 ) + 2 ;
-    //
-    //        // Overall we have a sequence of a certain length
-    builder[0] = 0x30;    // ASN.1 encoding representing a SEQUENCE
-    //        // Build up overall size made up of -
-    //        // size of OID + size of bitstring encoding + size of actual key
-    size_t i = sizeof(_encodedRSAEncryptionOID) + 2 + bitstringEncLength + [publicKeyData length];
-    size_t j = encodeLength(&builder[1], i);
-    [encKey appendBytes:builder length:j +1];
-
-    // First part of the sequence is the OID
-    [encKey appendBytes:_encodedRSAEncryptionOID
-                 length:sizeof(_encodedRSAEncryptionOID)];
-
-    // Now add the bitstring
-    builder[0] = 0x03;
-    j = encodeLength(&builder[1], [publicKeyData length] + 1);
-    builder[j+1] = 0x00;
-    [encKey appendBytes:builder length:j + 2];
-
-    // Now the actual key
-    [encKey appendData:publicKeyData];
-
-    return encKey;
-}
-
-size_t encodeLength(unsigned char * buf, size_t length) {
-
-    // encode length in ASN.1 DER format
-    if (length < 128) {
-        buf[0] = length;
-        return 1;
-    }
-
-    size_t i = (length / 256) + 1;
-    buf[0] = i + 0x80;
-    for (size_t j = 0 ; j < i; ++j) {
-        buf[i - j] = length & 0xFF;
-        length = length >> 8;
-    }
-
-    return i + 1;
 }
 
 @end
